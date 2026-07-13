28db8612eb98886b19e9dea6455132995dd8b37b
gagahsyuja <gghssa1@gmail.com>
2026-07-13 22:19:25 +0700

    feat: Add models and its migrations

---
+++ app/Models/Kategori.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Kategori extends Model
+{
+    protected $fillable = [
+        'nama'
+    ];
+
+    public function events()
+    {
+        return $this->hasMany(Event::class);
+    }
+}
